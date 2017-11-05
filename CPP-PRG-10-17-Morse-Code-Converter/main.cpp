005d4c5bfd1c17e10a1cc024e349b7e4588d349d
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-11-05 12:14:22 -0500

    Prompt written.

--- CPP-PRG-10-17-Morse-Code-Converter/main.cpp
+++ CPP-PRG-10-17-Morse-Code-Converter/main.cpp
@@ -5,11 +5,21 @@
 //  Created by Keith Smith on 11/5/17.
 //  Copyright © 2017 Keith Smith. All rights reserved.
 //
+//  Morse code is a code where each letter of the English alphabet, each digit, and various
+//  punctuation characters are represented by a series of dots and dashes. Table 10-10
+//  shows part of the code.
+//
+//  Write a program that asks the user to enter a string then converts that string to Morse
+//  code.
+//
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main()
+{
+    
+    
     return 0;
 }
